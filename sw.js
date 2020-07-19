f47defa1ab7a5f5a3a6fabac287e138754838e31
Carl Nygard <cjnygard@gmail.com>
2020-07-19 14:45:37 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4a0eef429c7419f4e034.js"
+    "url": "webpack-runtime-e27b783dc173dd2eaf16.js"
   },
   {
     "url": "styles.af1232871aadd97dd267.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4a778900b026565975cbcd4feceba62e"
+    "revision": "386d7e64bfba2bd5803e4781e769c3dc"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9875e2473a7c518755f.js"
